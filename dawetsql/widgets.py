2bf315134468147dcf56ed89afe074d7e2ea14d3
wakataw <me@agungpratama.id>
2018-05-31 22:24:30 +0700

    fix variable and method name, add method documentation

--- dawetsql/widgets.py
+++ dawetsql/widgets.py
@@ -1,59 +1,86 @@
-from ipywidgets import Textarea, Layout, Button, HBox, VBox, Output, Dropdown, Label
+from IPython.display import display
+from ipywidgets import Textarea, Button, HBox, VBox, Output, Dropdown, Label
 
 
 class SchemaExplorer(object):
-    __query = '''SELECT 
-    position,
-    schemaname,
-    tablename,
-    name,
-    javaclass
-FROM columns
-WHERE vdbname = 'DJPOLAP2'
-ORDER BY
-    schemaname,
-    tablename,
-    position'''
 
     def __init__(self, dawetsql):
+        '''
+        Initialize SchemaExplorer
+        :param dawetsql: OdbcSqlMagics object
+        '''
         self.__odbc = dawetsql
         self.out = Output()
         self.box = VBox()
-        self.get_dbschema()
-        self.schema_name = Dropdown(options=['Pilih Schema'] + self.dbschema.schemaname.unique().tolist())
-        self.tables_name = Dropdown()
-        self.detil_table = Button(description='Detil Table', button_style='success')
+        self.get_schemas()
+        self.schema_list = Dropdown(options=['Choose Schema'] + self.schemas.schemaname.unique().tolist())
+        self.table_list = Dropdown()
+        self.table_detail = Button(description='Table Detail', button_style='success')
         self.query_area = Textarea(
             placeholder='Query',
             disabled=True
         )
 
-        self.schema_name.observe(self.on_schema_change, names='value')
-        self.detil_table.on_click(self.on_detiltable_click)
+        self.schema_list.observe(self.on_schema_change, names='value')
+        self.table_detail.on_click(self.on_table_detail_click)
 
         display(Label('Schema Explorer'))
-        display(HBox([self.schema_name, self.tables_name, self.detil_table]))
+        display(HBox([self.schema_list, self.table_list, self.table_detail]))
         display(self.box)
 
-    def get_dbschema(self):
+    def get_schemas(self):
+        '''
+        Get database schema, table, and columns
+        :return:
+        '''
+        query = '''SELECT 
+            position,
+            schemaname,
+            tablename,
+            name,
+            javaclass
+        FROM columns
+        WHERE vdbname = 'DJPOLAP2'
+        ORDER BY
+            schemaname,
+            tablename,
+            position'''
+
         with self.out:
             self.__odbc.odbc_connect('djpolap2')
-            self.dbschema = self.__odbc.get_dataframe(self.__query)
+            self.schemas = self.__odbc.get_dataframe(query)
             self.__odbc.odbc_disconnect()
 
     def on_schema_change(self, change):
-        self.tables_name.options = ['Pilih Table'] + self.dbschema[
-            (self.dbschema.schemaname == change['new'])].tablename.unique().tolist()
+        '''
+        On Schema Dropdown change callback function
+        :param change: dropdown update
+        :return:
+        '''
+        self.table_list.options = ['Choose Table'] + self.schemas[
+            (self.schemas.schemaname == change['new'])].tablename.unique().tolist()
 
     def query_builder(self, schema, table, columns):
+        '''
+        Generate SQL select query
+        :param schema: schema name
+        :param table: table name
+        :param columns: table columns
+        :return: SQL select query string
+        '''
         query = "SELECT \n{}".format(',\n'.join(['    {}'.format(col) for col in columns]))[:-1]
         query += "\nFROM \n    {}.{}".format(schema, table)
         return query
 
-    def on_detiltable_click(self, arg):
+    def on_table_detail_click(self, arg):
+        '''
+        Detail Table Button callback function
+        :param arg: ipython widgets default arguments
+        :return:
+        '''
         self.out.clear_output()
-        detil = self.dbschema[(self.dbschema.schemaname == self.schema_name.value) & (
-                    self.dbschema.tablename == self.tables_name.value)].reset_index(drop=True)
+        detil = self.schemas[(self.schemas.schemaname == self.schema_list.value) & (
+                self.schemas.tablename == self.table_list.value)].reset_index(drop=True)
         self.query_area.value = self.query_builder(detil.schemaname.unique()[0], detil.tablename.unique()[0],
                                                    detil.name.tolist())
         with self.out:
