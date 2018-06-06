6b16ddc0a5654d10239dcbdf9972ffbb1cbcd790
wakataw <agungpratama1001@gmail.com>
2018-06-06 07:38:27 +0000

    update version

--- dawetsql/__init__.py
+++ dawetsql/__init__.py
@@ -1,6 +1,6 @@
 from .odbc_sql import OdbcSqlMagics
 
-__version__ = '0.0.1.dev4'
+__version__ = '0.0.1.dev5'
 
 def load_ipython_extension(ipython):
     ipython.register_magics(OdbcSqlMagics)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ipython-dawet-sql',
-    version='0.0.1.dev4',
+    version='0.0.1.dev5',
     packages=['dawetsql'],
     url='https://gitlab.com/wakataw/ipython-dawet-sql',
     license='MIT',
