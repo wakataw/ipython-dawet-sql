aee80ecc5cc04ce2da042a29b2ee63b3d10b3d93
wakataw <agungpratama1001@gmail.com>
2018-08-15 04:15:50 +0000

    change email and add author

--- dawetsql/__init__.py
+++ dawetsql/__init__.py
@@ -1,6 +1,7 @@
 from .odbc_sql import OdbcSqlMagics
 
 __version__ = '0.0.1.dev5'
+__author__ = 'Agung Pratama'
 
 def load_ipython_extension(ipython):
     ipython.register_magics(OdbcSqlMagics)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://gitlab.com/wakataw/ipython-dawet-sql',
     license='MIT',
     author='Agung Pratama',
-    author_email='me@agungpratama.id',
+    author_email='agungpratama1001[at]gmail.com',
     description='Ipython ODBC SQL Magic for Dawet',
     install_requires=[
         'pandas',
